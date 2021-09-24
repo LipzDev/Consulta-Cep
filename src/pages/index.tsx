cebc5c11a6f515f892faf1a1e46fc2039ce9b3d7
Filipe Gomes <filipe.gt1998@outlook.com>
2021-09-24 20:20:24 -0300

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,8 @@
+import React, { useState } from "react";
+import Layout from "../components/Layout";
+
+const Home = (props: any) => {
+  return <Layout>// CONTEUDO //</Layout>;
+};
+
+export default Home;
