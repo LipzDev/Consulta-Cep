3c4efb533b7581237c99c8fafe7363be6ff89b03
Filipe Gomes <filipe.gt1998@outlook.com>
2021-09-25 02:40:44 -0300

    This commit adds the functionality to list saved data

--- src/templates/HomeTemplate/index.tsx
+++ src/templates/HomeTemplate/index.tsx
@@ -6,7 +6,16 @@ import * as Styles from "./styles";
 const Home = () => {
   const [cep, setCep] = useState();
   const [data, setData] = useState<any>();
-  const [locale, setLocale] = useState<String[]>([]);
+  const [locale, setLocale]: any = useState(() => {
+    if (typeof window !== "undefined") {
+      const itemInStorage = JSON.parse(localStorage.getItem("info") as any);
+      if (!itemInStorage) return [];
+      return itemInStorage;
+    }
+  });
+  const [favorite, setFavorite] = useState<any>();
+
+  // FUNÇÃO QUE FAZ O FETCH
 
   const zipCodeSearch = async (e: any) => {
     e.preventDefault();
@@ -20,32 +29,39 @@ const Home = () => {
     }
   };
 
+  // FUNÇÃO QUE SALVA OS DADOS
+
   const handleFavorite = () => {
     setLocale([...locale, data]);
   };
-
   useEffect(() => {
-    const getStorage = localStorage.getItem("info");
-    const storageContent = JSON.parse(getStorage!);
-
-    // if (storageContent.filter(el => el.cep !== data.cep)) {
-    // }
     localStorage.setItem("info", JSON.stringify(locale));
   }, [locale]);
+
+  // FUNÇÃO QUE PUXA DADOS SALVOS
+
+  const showFavorite = () => {
+    const getStorage = window.localStorage.getItem("info");
+    const storageContent = JSON.parse(getStorage!);
+    setFavorite(storageContent);
+  };
 
   return (
     <Layout>
       <Container>
+        {/* SERVE PARA OBTER O TEXTO DO INPUT */}
+
         <form onSubmit={(e: any) => zipCodeSearch(e)}>
           <input
             type="text"
             placeholder="CEP"
+            required
             onChange={(e: any) => setCep(e.target.value)}
           />
           <button>Pesquisar</button>
         </form>
 
-        {/* dados */}
+        {/* MOSTRA AS INFORMAÇÕES DA API */}
 
         {data && (
           <div>
@@ -53,10 +69,30 @@ const Home = () => {
             <p>Bairro: {data?.bairro}</p>
             <p>Cidade: {data?.localidade}</p>
             <p>Estado: {data?.uf}</p>
+            <br></br>
           </div>
         )}
 
-        <button onClick={() => handleFavorite()}>Favorite</button>
+        {/* MOSTRA OS CARDS FAVORITOS */}
+
+        {favorite?.length === 0 && (
+          <small>Você não tem nada em favoritos</small>
+        )}
+
+        {favorite?.map(fav => (
+          <div>
+            <p>Logradouro: {fav?.logradouro}</p>
+            <p>Bairro: {fav?.bairro}</p>
+            <p>Cidade: {fav?.localidade}</p>
+            <p>Estado: {fav?.uf}</p>
+            <br></br>
+          </div>
+        ))}
+
+        {/* ATIVA AS FUNÇÕES SALVAR E FAVORITAR */}
+
+        <button onClick={() => handleFavorite()}>Save</button>
+        <button onClick={() => showFavorite()}>Favorite</button>
       </Container>
     </Layout>
   );
